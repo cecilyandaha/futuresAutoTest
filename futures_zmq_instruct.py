d1c7f8c34dde193ea57ba65d3347f2b6d202eda0
楼珊 <lisalou@bitasset.com>
2020-09-11 14:32:47 +0800

    提交

--- futures_zmq_instruct.py
+++ futures_zmq_instruct.py
@@ -13,9 +13,15 @@ def to_decimal(f, p = 6):
 
 
 #参数更新推送
-def paramRefresh():
+def paramRefresh5009():
     datajson={}
     datajson['message_type'] = 5009
+    datajson['appl_id'] = 2
+    return datajson
+
+def paramRefresh5018():
+    datajson={}
+    datajson['message_type'] = 5018
     datajson['appl_id'] = 2
     return datajson
 
@@ -27,13 +33,23 @@ def core_super_userRefresh():
     datajson['load_type'] = 2
     return datajson
 
+#保证金修改指令
+def core_marginRefresh():
+    datajson={}
+    datajson['message_type'] = 5012
+    datajson['appl_id'] = 2
+    datajson['account_id'] = 0
+    datajson['variety_id'] = 12
+    datajson['contract_id'] = 0
+    return datajson
+
 if __name__ == '__main__':
     TRADE_SERVER_URL = 'tcp://192.168.1.211:20060'
     #TRADE_client_URL = 'tcp://192.168.1.150:20050'
     context = zmq.Context()
     socket = context.socket(zmq.DEALER)
     socket.connect(TRADE_SERVER_URL)
-    datajson = core_super_userRefresh()
+    datajson = core_marginRefresh()
     str_json = json.dumps(datajson).encode('utf-8')
     print(str_json)
     socket.send(str_json)
--- futures_zmq_receive.py
+++ futures_zmq_receive.py
@@ -10,5 +10,5 @@ socket.connect("tcp://192.168.1.211:20051")
 
 while True:
     json = eval(socket.recv())
-    if json['message_type'] not in [4,7,13]  :
-        print (json)
+    if json['message_type'] not in [4,7,13]:
+           print (json)
